026170771e84dbe1cde45d6f7f17bcefd7d1c72e
shahd madhoun <shahdem100@gmail.com>
2021-01-21 11:41:38 +0300

    mobile version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit  } from '@angular/core';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
 import { CountriesService } from './services/countries.service';
+import { readSync } from 'fs';
 
 
 
@@ -14,13 +15,14 @@ export interface city {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
   cityData:any;
   selected:any;
+  time = 0;
   constructor(private data: CountriesService) {
-    console.log(environment.url);
+    
   }
   ngOnInit(): void {
     this.data.getCity().subscribe(d => {
@@ -28,10 +30,21 @@ export class AppComponent implements OnInit{
       console.log(this.cityData);
     });
   }
-
   select(value: any) {
     this.selected = value;
     console.log(this.selected);
   }
   
+
+  changeIcon() {
+    if (this.time == 0) {
+      this.time +=1;
+      return this.time;
+    } else {
+      this.time -=1;
+      return this.time;
+    }
+   }
+
+   
 }
